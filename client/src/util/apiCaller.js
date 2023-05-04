61f4aab9363a45f5bf4b0e3a766b0d19145f7a7b
svalverde <sv@lexify.es>
2023-05-04 17:44:15 +0200

    uncommited token bearer

--- client/src/util/apiCaller.js
+++ client/src/util/apiCaller.js
@@ -1,14 +1,25 @@
 import fetch from 'isomorphic-fetch';
+import { JWT_TOKEN } from '../User/UserActions';
 
 export const API_URL = 'http://localhost:3000/api';
 
 export default async (endpoint, method = 'get', body) => {
-  return fetch(`${API_URL}/${endpoint}`, {
+
+  const headers = {
     headers: { 'content-type': 'application/json' },
     method,
     body: JSON.stringify(body),
-  })
-  .then(response => response.json().then(json => ({ json, response })))
+  }
+
+  const jwtToken = localStorage.getItem(JWT_TOKEN);
+
+  if(jwtToken) headers.headers.Authorization = `Bearer ${jwtToken}`;
+
+  return fetch(`${API_URL}/${endpoint}`, headers)
+  .then(response => {
+    // TODO Handle non-json responses (e.g. passport errors) or handle in the server
+    return response.json().then(json => ({ json, response }))}
+    )
   .then(({ json, response }) => {
     if (!response.ok) {
       return Promise.reject(json);
